9796b18e3598d991b2dd10d42dd2abf2da2b2da8
gneve <gneve@student.s19.be>
2021-02-23 14:05:11 +0000

    push

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,12 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:48:13 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/23 13:28:52 by gneve            ###   ########.fr       */
+/*   Updated: 2021/02/23 14:04:58 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-int			ft_printf(const char *input, ...)
+int	ft_printf(const char *string, ...)
 {
-	const char	*save;
-	va_list		args;
-	int			char_count;
-
-	save = ft_strdup(input);
-	char_count = 0;
-	va_start(args, input);
-	char_count += ft_treat_save(save, args);
-	va_end(args);
-	free((char *)save);
-	return (char_count);
 }
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:46:31 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/23 13:20:23 by gneve            ###   ########.fr       */
+/*   Updated: 2021/02/23 13:42:49 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,10 @@
 
 # include <stdargs.h>
 # include <stdlib.h>
+
+typedef struct s_flags
+{
+    int counter;
+    int type;
+    int minus;
+}
