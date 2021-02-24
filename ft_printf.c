521363d2784fdd1a0c1d6cdff8cb192bb7834c4a
gneve <gneve@student.s19.be>
2021-02-24 23:26:47 +0000

    change the var name

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:48:13 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/24 17:52:23 by gneve            ###   ########.fr       */
+/*   Updated: 2021/02/24 23:25:56 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@ int ft_putchar(int c) {
 	return write(0, a, 1);
 }
 
-static int ft_printf_aux(const char *fmt, va_list ap, int len);
+static int ft_printf_lenght(const char *fmt, va_list ap, int len);
 
-static int ft_print_c(const char *fmt, va_list ap, int len) {
+static int ft_printf_c(const char *fmt, va_list ap, int len) {
 	int c = va_arg(ap, int);
 	ft_putchar(c);
-	return ft_printf_aux(fmt, ap, len + 1);
+	return ft_printf_lenght(fmt, ap, len + 1);
 }
 
-static int ft_putnum(unsigned long long n, unsigned int base, const char *digits) {
+static int ft_putnumb_base(unsigned long long n, unsigned int base, const char *digits) {
 	int res = 1;
 	if (n >= base)
-		res += ft_putnum(n / base, base, digits);
+		res += ft_putnumb_base(n / base, base, digits);
 	ft_putchar(digits[n % base]);
 	return res;
 }
 
-static int ft_print_d(const char *fmt, va_list ap, int len) {
+static int ft_printf_d(const char *fmt, va_list ap, int len) {
 	int n = va_arg(ap, int);
 	unsigned long long u;
 	if (n < 0) {
@@ -44,35 +44,35 @@ static int ft_print_d(const char *fmt, va_list ap, int len) {
 	} else {
 		u = n;
 	}
-	len += ft_putnum(u, 10, "0123456789");
-	return ft_printf_aux(fmt, ap, len);
+	len += ft_putnumb_base(u, 10, "0123456789");
+	return ft_printf_lenght(fmt, ap, len);
 }
 
-static int ft_print_o(const char *fmt, va_list ap, int len) {
+static int ft_printf_o(const char *fmt, va_list ap, int len) {
 	unsigned int n = va_arg(ap, unsigned int);
-	len += ft_putnum(n, 8, "01234567");
-	return ft_printf_aux(fmt, ap, len);
+	len += ft_putnumb_base(n, 8, "01234567");
+	return ft_printf_lenght(fmt, ap, len);
 }
 
-static int ft_print_u(const char *fmt, va_list ap, int len) {
+static int ft_printf_u(const char *fmt, va_list ap, int len) {
 	unsigned int n = va_arg(ap, unsigned int);
-	len += ft_putnum(n, 10, "0123456789");
-	return ft_printf_aux(fmt, ap, len);
+	len += ft_putnumb_base(n, 10, "0123456789");
+	return ft_printf_lenght(fmt, ap, len);
 }
 
-static int ft_print_x(const char *fmt, va_list ap, int len) {
+static int ft_printf_x(const char *fmt, va_list ap, int len) {
 	unsigned int n = va_arg(ap, unsigned int);
-	len += ft_putnum(n, 16, "0123456789abcdef");
-	return ft_printf_aux(fmt, ap, len);
+	len += ft_putnumb_base(n, 16, "0123456789abcdef");
+	return ft_printf_lenght(fmt, ap, len);
 }
 
-static int ft_print_X(const char *fmt, va_list ap, int len) {
+static int ft_printf_X(const char *fmt, va_list ap, int len) {
 	unsigned int n = va_arg(ap, unsigned int);
-	len += ft_putnum(n, 16, "0123456789ABCDEF");
-	return ft_printf_aux(fmt, ap, len);
+	len += ft_putnumb_base(n, 16, "0123456789ABCDEF");
+	return ft_printf_lenght(fmt, ap, len);
 }
 
-static int ft_print_s(const char *fmt, va_list ap, int len) {
+static int ft_printf_s(const char *fmt, va_list ap, int len) {
 	const char *s = va_arg(ap, const char *);
 	if (s == NULL) {
 		s = "(null)";
@@ -81,23 +81,23 @@ static int ft_print_s(const char *fmt, va_list ap, int len) {
 		ft_putchar(*s++);
 		len++;
 	}
-	return ft_printf_aux(fmt, ap, len);
+	return ft_printf_lenght(fmt, ap, len);
 }
 
 typedef int (*ft_print_dispatch_f)(const char *fmt, va_list ap, int len);
 
 static ft_print_dispatch_f const ft_print_dispatch[256] = {
-	['c'] = ft_print_c,
-	['d'] = ft_print_d,
-	['i'] = ft_print_d,
-	['o'] = ft_print_o,
-	['u'] = ft_print_u,
-	['x'] = ft_print_x,
-	['X'] = ft_print_X,
-	['s'] = ft_print_s,
+	['c'] = ft_printf_c,
+	['d'] = ft_printf_d,
+	['i'] = ft_printf_d,
+	['o'] = ft_printf_o,
+	['u'] = ft_printf_u,
+	['x'] = ft_printf_x,
+	['X'] = ft_printf_X,
+	['s'] = ft_printf_s,
 };
 
-static int ft_printf_aux(const char *fmt, va_list ap, int len) {
+static int ft_printf_lenght(const char *fmt, va_list ap, int len) {
 	int c;
 
 	while (*fmt)
@@ -122,23 +122,23 @@ static int ft_printf_aux(const char *fmt, va_list ap, int len) {
 }
 
 int ft_vprintf(const char *fmt, va_list ap) {
-	return ft_printf_aux(fmt, ap, 0);
+	return ft_printf_lenght(fmt, ap, 0);
 }
 
 int ft_printf(const char *string, ...) {
 	va_list ap;
 	int n;
 	va_start(ap, string);
-	n = ft_printf_aux(string, ap, 0);
+	n = ft_printf_lenght(string, ap, 0);
 	va_end(ap);
 	return n;
 }
 
 #include <stdio.h>
 
-#define TEST "%% test"
+#define TEST "%s", "gregoire neve"
 int main(void)
 {
+    printf("\n stdio  %d\n", printf(TEST));
   ft_printf("\n FT_42  %d \n", ft_printf(TEST));
-  printf("\n stdio %d\n", printf(TEST));
 }
