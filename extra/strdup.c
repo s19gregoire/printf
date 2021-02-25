8d15e3af2f5592c7391b3e83e1173a311fb49ccb
gneve <gneve@student.s19.be>
2021-02-25 19:06:21 +0000

    gregoire

--- extra/strdup.c
+++ extra/strdup.c
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   strdup.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/25 18:23:44 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/25 19:02:50 by gneve            ###   ########.fr       */
+/*   Created: 2021/02/25 19:04:55 by gneve             #+#    #+#             */
+/*   Updated: 2021/02/25 19:05:57 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_print.h"
 
-int	ft_printf(const char *string, ...)
+char	*ft_strdup(const char *src)
 {
-	t_print formater;
-	va_list	format;
+	int		i;
+	int		len;
+	char	*str;
+
+	len = 0;
+	while (src[len])
+		len++;
+	if (!(str = (char*)malloc(sizeof(*str) * (len + 1))))
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
--- includes/ft_print.h
+++ includes/ft_print.h
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 18:30:26 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/25 18:56:49 by gneve            ###   ########.fr       */
+/*   Updated: 2021/02/25 19:06:18 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 # define FT_PRINT_H
 
 # include "stdarg.h"
+# include <stdlib.h>
 
 typedef struct	s_print
 {
 	int			width;
+	int			counter;
 }				t_print;
 
+char	*ft_strdup(const char *src);
 #endif
