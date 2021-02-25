c0c6cce58e27c0e6aa49a85a62e289350331ee4c
gneve <gneve@student.s19.be>
2021-02-25 18:42:42 +0000

    gregoire

--- includes/ft_print.h
+++ includes/ft_print.h
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/24 17:00:33 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/25 14:14:30 by gneve            ###   ########.fr       */
+/*   Created: 2021/02/25 18:30:26 by gneve             #+#    #+#             */
+/*   Updated: 2021/02/25 18:32:16 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-#include <stdarg.h>
-#include <unistd.h>
-
-enum e_flags
-{
-    F_STAR = 1,
-    F_DOT = (1<<1),
-    F_ZERO = (1<<2),
-    F_NEG = (1<<3)
-};
-
-int ft_printf(const char *string, ...);
-
+#ifndef FT_PRINT_H
 #endif
