b94f6ae9e1ee30acdca30dc0bf797e31567f7cdd
gneve <gneve@student.s19.be>
2021-02-25 21:42:05 +0000

    d

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 18:23:44 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/25 21:40:21 by gneve            ###   ########.fr       */
+/*   Updated: 2021/02/25 21:41:44 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int				ft_printf(const char *fmt, ...)
 	t_print		data;
 	const char	*stringdata;
 
-	data.strpos = 0;
-	data.wordcount = 0;
+	data = (t_print){0, NULL, 0, 0, 0, 0};
 	while (fmt[data.strpos])
 	{
 		if (fmt[data.strpos] == '%')
