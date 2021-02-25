b7a55655e7669da9da4d0e31ac362a4e41d29ee4
gneve <gneve@student.s19.be>
2021-02-25 19:54:43 +0000

    gregoire

--- parser/ft_flags_parse.c
+++ parser/ft_flags_parse.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_flags_parse.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/25 18:23:44 by gneve             #+#    #+#             */
-/*   Updated: 2021/02/25 19:53:43 by gneve            ###   ########.fr       */
+/*   Created: 2021/02/25 19:50:50 by gneve             #+#    #+#             */
+/*   Updated: 2021/02/25 19:54:39 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_print.h"
-
-int	ft_printffd(const char *stringdata, va_list format)
-{
-	t_print formater;
-
-	formater.counter = 0;
-}
-
-int	ft_printf(const char *string, ...)
-{
-	t_print				formater;
-	const char			*stringdata;
-
-	formater.counter += ft_printffd(stringdata, formater.format);
-}
